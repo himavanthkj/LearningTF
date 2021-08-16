601ee0cfb112d127cc7a614c1963b0a98aba867d
himavanthkj <you@example.com>
2021-08-16 22:44:42 +0530

    Adding cidr for subnets

--- variables.tf
+++ variables.tf
@@ -3,3 +3,49 @@ variable "instance_name" {
   type        = string
   default     = "ExampleAppServerInstance"
 }
+
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+    "10.0.9.0/24",
+    "10.0.10.0/24",
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24",
+    "10.0.14.0/24",
+    "10.0.15.0/24",
+    "10.0.16.0/24"
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24",
+    "10.0.109.0/24",
+    "10.0.110.0/24",
+    "10.0.111.0/24",
+    "10.0.112.0/24",
+    "10.0.113.0/24",
+    "10.0.114.0/24",
+    "10.0.115.0/24",
+    "10.0.116.0/24"
+  ]
+}
